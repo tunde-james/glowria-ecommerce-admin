5b3bc59ff9876e8307275f37ae76ff54ebfb4173
Tunde James <tundejames04@gmail.com>
2023-12-16 19:20:33 +0100

    Create a modal using shadcn ui Create a reusable modal from shadcn modal Add global state to open and close the modal with Zustand

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
-import { UserButton } from '@clerk/nextjs';
+'use client';
+
+import { useEffect } from 'react';
+
+import { useStoreModal } from '@/hooks/use-store-modal';
 
 function SetupPage() {
-  return (
-    <div className="p-4">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 }
 
 export default SetupPage;
